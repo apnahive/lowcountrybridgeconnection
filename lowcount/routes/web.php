b2c14e3b793314599b5d1c57bf918bcd7693b89b
samargurjar <samar.gurjar@yahoo.com>
2017-05-05 23:08:05 +0530

    multiple user authentication

--- lowcount/routes/web.php
+++ lowcount/routes/web.php
@@ -18,9 +18,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/superadmin', 'SuperadminController@index')->name('superadmin');
-Route::get('/unitadmin', 'UnitadminController@index')->name('unitadmin');
-Route::get('/teacher', 'TeacherController@index')->name('teacher');
 Route::get('/manager', 'ManagerController@index')->name('manager');
 Route::get('/email', 'HomeController@email')->name('sendEmail');
-
+Route::get('/superadmin/login', 'SuperAdminLoginController@showLoginForm')->name('superadmin.login');
+Route::post('/superadmin/login', 'SuperAdminLoginController@login')->name('superadmin.login.submit');
+Route::get('/superadmin', 'SuperadminController@index')->name('superadmin');
+Route::get('/unitadmin/login', 'UnitAdminLoginController@showLoginForm')->name('unitadmin.login');
+Route::post('/unitadmin/login', 'UnitAdminLoginController@login')->name('unitadmin.login.submit');
+Route::get('/unitadmin', 'UnitadminController@index')->name('unitadmin');
+Route::get('/teacher/login', 'TeacherLoginController@showLoginForm')->name('teacher.login');
+Route::post('/teacher/login', 'TeacherLoginController@login')->name('teacher.login.submit');
+Route::get('/teacher', 'TeacherController@index')->name('teacher');
+Route::get('/manager/login', 'ManagerLoginController@showLoginForm')->name('manager.login');
+Route::post('/manager/login', 'ManagerLoginController@login')->name('manager.login.submit');
+Route::get('/manager', 'ManagerController@index')->name('manager');
